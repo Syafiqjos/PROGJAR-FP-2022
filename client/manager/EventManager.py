862bee561ae71234027276efa0b82261dd314f91
Syafiqjos <syafiqjos@gmail.com>
2022-05-22 19:26:45 +0700

    feat(client): add check click any method

--- client/manager/EventManager.py
+++ client/manager/EventManager.py
@@ -4,6 +4,11 @@ class EventManager():
 	def __init__(self):
 		pass
 
+	def checkOnClickAny(self, event):
+		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+			return True
+		return False
+
 	def checkOnClick(self, event, sprite):
 		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and sprite.rect.collidepoint(event.pos):
 			return True
