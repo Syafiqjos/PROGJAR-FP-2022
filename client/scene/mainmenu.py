c21ce559381c2358d5c779132f3bc4fabcf024bc
Syafiqjos <syafiqjos@gmail.com>
2022-05-22 09:40:04 +0700

    feat(client): implement base main menu buttons

--- client/scene/mainmenu.py
+++ client/scene/mainmenu.py
@@ -16,28 +16,32 @@ class MainMenu():
 		self.awake()
 
 	def awake(self):
-		self.robot = Sprite(self.screen, (0, 0), (0.2, 0.2), 'Assets/robotball/skeleton-animation_01.png')
-		# self.robot.events['onclick'] = self.exitGame;
+		self.playButton = Sprite(self.screen, (100, 100), (1, 1), 'Assets/gameicons/PNG/White/1x/forward.png')
+		self.exitButton = Sprite(self.screen, (300, 100), (1, 1), 'Assets/gameicons/PNG/White/1x/power.png')
 
-		self.sprites.append(self.robot)
+		self.sprites.append(self.playButton)
+		self.sprites.append(self.exitButton)
 		
 
 	def events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.gameManager.running = False
-			if self.eventManager.checkOnClick(event, self.robot):
-				print('hapen')
+			if self.eventManager.checkOnClick(event, self.playButton):
+				print('play game')
+				self.playGame()
+			elif self.eventManager.checkOnClick(event, self.exitButton):
+				print('exit game')
+				self.exitGame()
 
 	def render(self):
-		self.screen.fill((255, 255, 255))
-		pygame.draw.circle(self.screen, (0, 0, 255), (250, 250), 75)
+		self.screen.fill((0, 0, 0))
 		
 		for sprite in self.sprites:
 			sprite.render()
 
 	def playGame(self):
-		self.gameManager.scene = MainMenu(this.gameManager)
+		self.gameManager.scene = MainMenu(self.gameManager)
 
 	def exitGame(self):
 		self.gameManager.running = False
