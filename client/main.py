dc35a593d6d4e25f5522fa404a0ef9140af88fda
Syafiqjos <syafiqjos@gmail.com>
2022-05-22 14:27:50 +0700

    feat(client): implement gameplay scene in main

--- client/main.py
+++ client/main.py
@@ -3,12 +3,14 @@ from Manager.DataManager import DataManager
 from Manager.EventManager import EventManager
 from Scene.MainMenu import MainMenu
 from Scene.LobbyMenu import LobbyMenu
+from Scene.GameplayScene import GameplayScene
 
 dataManager = DataManager()
 eventManager = EventManager()
 gameManager = GameManager(dataManager, eventManager, [720, 480])
 gameManager.registerScene('MainMenu', MainMenu);
 gameManager.registerScene('LobbyMenu', LobbyMenu);
+gameManager.registerScene('GameplayScene', GameplayScene);
 
-gameManager.loadScene('MainMenu')
+gameManager.loadScene('GameplayScene')
 gameManager.run()
