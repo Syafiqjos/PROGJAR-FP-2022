3ddcbabec62298dbbdd08204f67b3246a1da1806
rizadwiandhika <rizadwiandhika@gmail.com>
2022-05-26 17:11:25 +0700

    feat(server): cancel find match

--- server/controllers/cancel.py
+++ server/controllers/cancel.py
@@ -1,2 +1,10 @@
-def cancel_find_match(*args, **kwargs):
-    return
+import socket
+
+
+def cancel_find_match(
+    client: socket.socket, plant_queue: list, zombie_queue: list, *args, **kwargs
+):
+    if client in plant_queue:
+        plant_queue.remove(client)
+    if client in zombie_queue:
+        zombie_queue.remove(client)
