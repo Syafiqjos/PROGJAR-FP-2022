f2a965c9ceda446375088aac854f7ae8f3094ed6
Syafiqjos <syafiqjos@gmail.com>
2022-05-30 19:01:37 +0700

    refactor(client): move game script into respectly own namespace

---
+++ client/Game/Gameplay.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('..')
+
+from manager.GameManager import GameManager
+from manager.DataManager import DataManager
+from manager.EventManager import EventManager
+from scene.MainMenu import MainMenu
+from scene.LobbyMenu import LobbyMenu
+from scene.GameplayScene import GameplayScene
+
+def game(dataManager, socketManager, accountSocket, gameSocket):
+	eventManager = EventManager()
+	gameManager = GameManager(dataManager, eventManager, [720, 480], socketManager, accountSocket, gameSocket)
+	gameManager.registerScene('MainMenu', MainMenu);
+	gameManager.registerScene('LobbyMenu', LobbyMenu);
+	gameManager.registerScene('GameplayScene', GameplayScene);
+	# gameManager.registerScene('ScoreMenu', ScoreMenu);
+
+	gameManager.loadScene('GameplayScene')
+	gameManager.run()
