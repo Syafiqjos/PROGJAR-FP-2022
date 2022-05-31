8550240473c19bfb1218fd98696c7df6c495b271
anakpayah <afifansyafaqi@gmail.com>
2022-05-31 16:37:37 +0700

    update(client): ScoreMenu updating (not cleared yet)

--- client/scene/ScoreMenu.py
+++ client/scene/ScoreMenu.py
@@ -7,53 +7,67 @@ from Library.Display import Display
 import pygame
 
 class ScoreMenu(): 
-    def __init__(self, gameManager, status):
+    def __init__(self, gameManager):
         self.gameManager = gameManager
         self.dataManager = self.gameManager.dataManager 
         self.eventManager = self.gameManager.eventManager 
         self.screen = self.gameManager.screen 
         self.sprites = {'SCORE': [], 'PLANT': [], 'ZOMBIE': []}
         self.object = {}
-        self.status = status
+        self.dataManager.set('game_winner', 'plant')
+        self.status = self.dataManager.get('game_winner')
 
         self.awake()
-        self.awakeScore(status)
+        self.awakeScore()
 
     def awake(self):
-        self.state = 'SCORE'
+        if (self.status == 'plant'):
+            self.state = 'PLANT'
+        elif (self.status == 'zombie'):
+            self.state = 'ZOMBIE'
 
-        self.object = Sprite(
+        self.lobbyButton = Sprite(
             self.screen, (200,300), (1,1), "Assets/gameicons/PNG/White/1x/power.png"
         )
         self.mainMenuButton = Sprite(
             self.screen, (500,300), (1,1), "Assets/gameicons/PNG/White/1x/power.png"
         )
 
+        print("masuk awake")
+
         self.sprites[self.state].append(self.lobbyButton)
         self.sprites[self.state].append(self.mainMenuButton)
 
-    def awakeScore(self, status):
-        if (status):
-            self.state = 'PLANT'
-        else:
-            self.state = 'ZOMBIE'
+
+    def awakeScore(self):
+        print("masuk awakeScore")
         
-        self.object.score = Display (
-            'Congratulation!', self.screen, (300,100), 48, (255,255,255)
+        self.object['score'] = Display (
+            'Congratulation!', self.screen, (300,100), 52, (255,255,255)
+        )
+
+        self.object ['scoreWinner'] = Display (
+            (self.state + 'Winner!'), self.screen, (300,200), 48, (255,255,255)
         )
     
-    def event(self):
-        for event in pygames.event.get():
+    def events(self):
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("Quiting the Game")
                 self.exitGame()
             if self.eventManager.checkOnClick(event, self.lobbyButton):
                 print("going back to Lobby.")
                 self.backToLobby()
-            elif self.eventManager.checkOnClick(event, self.mainMenuButtom):
+            elif self.eventManager.checkOnClick(event, self.mainMenuButton):
                 print("going back to main menu...")
                 self.goToMainMenu()
 
+    def render(self):
+        self.screen.fill((0, 0, 0)) 
+        # print("masuk re")
+        for sprite in self.sprites[self.state]:
+            # print("masuk render2")
+            sprite.render()
 
     def backToLobby(self):
         self.gameManager.loadScene('LobbyMenu')
