a2987ed00406878837f45f1845cb292214e12f81
Syafiqjos <syafiqjos@gmail.com>
2022-06-02 23:32:53 +0700

    feat(client): socket sender add optional sync receive on send method

--- client/Library/SocketSender.py
+++ client/Library/SocketSender.py
@@ -2,7 +2,9 @@ class SocketSender():
 	def __init__(self, socketManager):
 		self.socketManager = socketManager
 
-	def send(self, data):
+	def send(self, data, syncReceive = True):
 		self.socketManager.send(data)
+		if syncReceive != True:
+			return None
 		data = self.socketManager.receive()
 		return data
