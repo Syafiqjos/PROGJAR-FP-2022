a9c51a23f089f83861274827cb3541181283d391
Syafiqjos <syafiqjos@gmail.com>
2022-06-15 15:17:27 +0700

    fix(client): readd display class

---
+++ client/Library/Display.py
@@ -0,0 +1,34 @@
+import string
+import pygame
+
+class Display():
+
+    def __init__(self, text, screen, position, size=32, color=(255, 255, 255)):
+        self.isRender = True
+
+        self.screen = screen #game load screen
+        self.color = color # (R,G,B)
+        self.size = size
+        self.font = pygame.font.Font('Assets/Font/Sunflower.otf', self.size)
+        self.setText(str(text))
+        self.position = position
+        self.texting()
+
+
+    def texting(self):
+        self.textSurface = self.font.render(self.text, True, self.color)
+        self.textRect = self.textSurface.get_rect()
+        self.textRect.center = self.position
+
+    def setText(self, text):
+        self.text = text
+    
+    def awake(self):
+        self.screen.blit(self.textSurface,self.textRect)
+
+    def update(self):
+        pass
+
+    def render(self):
+        self.texting()
+        self.screen.blit(self.textSurface,self.textRect)
